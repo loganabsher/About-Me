a477f043fc67d1da9123d6adfcabe703c5d0975e
Patricia Antoinette Mangune <patriciamangune93@gmail.com>
2017-01-03 17:15:14 -0800

    Added questions to guessing game/console

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 'use strict';
 
-var answer1 =  confirm('Are you ready?');
+alert('Are you ready?');
+var userAnswer = prompt('QUESTION 1').toLowerCase();
 
-if (answer1 === true) {
-  console.log('Woo! User is ready!');
-} else {
-  console.log('Sorry, too bad...');
+if (userAnswer === 'yes') {
+  console.log('IF YES RESPONSE');
+} else{
+  console.log('IF NO RESPONSE');
+}
+
+var userAnswer = prompt('QUESTION 2').toLowerCase();
+
+if (userAnswer === 'yes') {
+  console.log('IF YES RESPONSE');
+} else{
+  console.log('IF NO RESPONSE');
+}
+
+var userAnswer = prompt('I wonder, did Chewbacca get the junior dev job that he interviewed for?').toLowerCase();
+
+if (userAnswer === 'yes') {
+  console.log('He must be pretty good for a wookie.');
+} else{
+  console.log('Ah, I suppose he\'s just a wookie.');
+}
+var userAnswer = prompt('QUESTION 4').toLowerCase();
+
+if (userAnswer === 'yes') {
+  console.log('IF YES RESPONSE');
+} else{
+  console.log('IF NO RESPONSE');
+}
+
+var userAnswer = prompt('QUESTION 5').toLowerCase();
+
+if (userAnswer === 'yes') {
+  console.log('IF YES RESPONSE');
+} else{
+  console.log('IF NO RESPONSE');
 }
