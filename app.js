d6fc679a0d03f88271a9090b866b371f84e20525
Patricia Antoinette Mangune <patriciamangune93@gmail.com>
2017-01-04 13:52:47 -0800

    lorem ipsum

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 'use strict';
 
-alert('Are you ready?');
-var userAnswer = prompt('QUESTION 1').toLowerCase();
+alert('Hello there~!');
+var userAnswer = prompt('I want to know. If the a site crashes on the deep web, does it make a sound?').toLowerCase();
 
 if (userAnswer === 'yes') {
-  console.log('IF YES RESPONSE');
+  console.log('S P O O P Y');
 } else{
-  console.log('IF NO RESPONSE');
+  console.log('It would be best if we stay out of there...');
 }
 
-var userAnswer = prompt('QUESTION 2').toLowerCase();
+var userAnswer = prompt('Do I seem like a brave person?').toLowerCase();
 
 if (userAnswer === 'yes') {
-  console.log('IF YES RESPONSE');
+  console.log('That\'s very nice of you to say! I hope I\'m at least, a little courageous when needed.');
 } else{
-  console.log('IF NO RESPONSE');
+  console.log('I\'m not! But maybe for cautionary purposes, I like to listen to scary stries onilne and watch scary movies.');
 }
 
-var userAnswer = prompt('I wonder, did Chewbacca get the junior dev job that he interviewed for?').toLowerCase();
+var userAnswer = prompt('Did Chewbacca get the junior dev job that he interviewed for?').toLowerCase();
 
 if (userAnswer === 'yes') {
   console.log('He must be pretty good for a wookie.');
 } else{
   console.log('Ah, I suppose he\'s just a wookie.');
 }
-var userAnswer = prompt('QUESTION 4').toLowerCase();
+var userAnswer = prompt('Sorry, these bad jokes are getting out of hand, wouldn\'t you say?').toLowerCase();
 
 if (userAnswer === 'yes') {
-  console.log('IF YES RESPONSE');
+  console.log('At least it\'s over now.');
 } else{
-  console.log('IF NO RESPONSE');
+  console.log('Comedy is harder than it seems!');
 }
 
-var userAnswer = prompt('QUESTION 5').toLowerCase();
+var userAnswer = prompt('Did you at least enjoy yourself?').toLowerCase();
 
 if (userAnswer === 'yes') {
-  console.log('IF YES RESPONSE');
+  console.log('Wonderful~! I\'m glad! Come back sometime, yeah?');
 } else{
-  console.log('IF NO RESPONSE');
+  console.log('Winter has been tough. I\'ve had to ration happiness, myself.');
 }
